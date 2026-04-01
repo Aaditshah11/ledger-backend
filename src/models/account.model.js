1e6db74de81f4500953ba1f2c986cc0b4c890212
Aaditshah11 <aaditshah2006@gmail.com>
2026-04-01 19:04:33 +0530

    accountModel

---
+++ src/models/account.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "Acc must be associated with a user"],
+      index: true,
+    },
+
+    status: {
+      type: String,
+      enum: {
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "Enter a valid account status",
+      },
+    },
+
+    currency: {
+      type: String,
+      required: [true, "Currency is required"],
+      default: "INR",
+    },
+  },
+  { timestamps: true },
+);
+
+accountSchema.index({ user: 1, status: 1 });
+
+const accountModel = mongoose.model("account", accountSchema);
+
+module.exports = accountModel;
