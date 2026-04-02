194366f6780302a8972fe15bb3ca9c89ce6ae67b
Aaditshah11 <aaditshah2006@gmail.com>
2026-04-02 22:31:23 +0530

    transaction Schema

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user.model");
 
 const authMiddleware = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
---
+++ src/models/transaction.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    fromAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "account",
+      required: [true, "From account is required"],
+      index: true,
+    },
+    toAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "account",
+      required: [true, "To account is required"],
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "complete", "failed", "reversed"],
+      default: "pending",
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount is required"],
+    },
+    idempotencyKey: {
+      type: String,
+      required: [true, "Idempotency key is required"],
+      unique: true,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const transactionModel = mongoose.model("transaction", transactionSchema);
+
+module.exports = transactionModel;
